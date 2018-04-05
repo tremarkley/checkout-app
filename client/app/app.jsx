5f345e7cf5e898419dff20206b3f1e53aae41b45
Harry Markley <tremarkley@gmail.com>
2018-04-05 11:23:49 -0700

    Create intitial components

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import AccountInfo from './components/accountInfo';
+import ContactInfo from './components/contactInfo';
+import PaymentInfo from './components/paymentInfo';
 
-const App = () => (
-  <p>Checkout App</p>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeForm: undefined,
+    };
+  }
+
+  render() {
+    switch (this.state.activeForm) {
+      case 'AccountInfo':
+        return <AccountInfo />;
+      case 'ContactInfo':
+        return <ContactInfo />;
+      case 'PaymentInfo':
+        return <PaymentInfo />;
+      default:
+        return <button>Checkout</button>;
+    }
+  }
+}
 
 export default App;
 
