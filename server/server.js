ff42e8d3c92fa87bd367056849fe417379d4cf94
Harry Markley <tremarkley@gmail.com>
2018-04-05 19:41:11 -0700

    Connect to mongo db before server is started

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const mongo = require('../db/db');
 
 const port = 3434;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.listen(port, () => {
-  console.log(`App listening on ${port}`);
-});
+mongo.connectMongo()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App listening on ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`unable to connect to mongo ${error}`);
+  });
 
